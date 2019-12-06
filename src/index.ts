2cf3c0e4b5a9d1d5323ee8fed0cba05f0d2c4c15
Caroline Lopes <caroline.lopesm@hotmail.com>
2019-12-06 10:35:08 -0300

    types

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+console.log ("oi")
+
+type UserAccount = {
+    name: String;
+    cpf: Number;
+    birth: String;
+  };
+
+  type moneyTransfer = {
+    clientName: String;
+    clientCpf: Number;
+    RecName: String;
+    RecCpf: Number;
+    value: Number;
+  };
