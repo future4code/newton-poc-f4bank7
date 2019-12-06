850b1300a1ebf308d4da42c8f86b18aafd8c0465
gamedesignbrasil <thiago@spellbook.com.br>
2019-12-06 14:59:48 -0300

    Atualizando primeiras validações

--- src/newacc.ts
+++ src/newacc.ts
@@ -1,26 +1,50 @@
-import { appendFile } from 'fs';
+import { appendFile, readFile } from 'fs';
+import * as moment from 'moment';
 
 const nome = process.argv[2];
 const cpf = process.argv[3];
 const nascimento = process.argv[4];
 
-const newCliente = {
+const client = {
   Nome: nome,
   CPF: cpf,
   Nascimento: nascimento,
-  Saldo: 0,
+  Saldo: 0
 };
+
+const allClients: any = [];
 
 function createAccount(): void {
 
-  appendFile("./clientes.json", JSON.stringify(newCliente, null, 4), (err) => {
+  allClients.push(client);
+
+  appendFile("./clientes.json", JSON.stringify(allClients, null, 4), (err) => {
     if (err) {
       console.log(err);
       return;
     };
-    console.log("Cliente cadastrado com sucesso!");
+    console.log();
+    console.log("\x1b[32m%s\x1b[0m", "Cliente cadastrado com sucesso!");
+    console.log();
   });
 
 }
 
-createAccount();
+function ageFromDateofBirth(dateOfBirth: any): number {
+  return moment().diff(dateOfBirth, 'years')
+}
+
+const userage = ageFromDateofBirth(nascimento);
+
+if (userage < 18) {
+  console.log();
+  console.log("\x1b[31m%s\x1b[0m", "Desculpe, apenas maiores de idade podem criar uma conta!");
+  console.log();
+} else {
+  createAccount();
+}
+
+
+
+
+
